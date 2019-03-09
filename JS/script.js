16df96957e11e236bd542566726cf86d911e772e
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-03-08 20:46:38 -0700

    Updated slider photos and added typed.js

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    $('#slides').superslides({
-        animation: 'fade',
-        play: 5000,
-        pagination: false
-    })
-})
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Photographer.", "Artist.", "Custom furniture upholsterer."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+});
