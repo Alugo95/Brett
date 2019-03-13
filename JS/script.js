cbfbdb902b291d9eb854cc8759fd83bcfce5576e
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-03-13 15:11:05 -0600

    bio and contact sections added and styled

--- JS/script.js
+++ JS/script.js
@@ -7,11 +7,28 @@ $(document).ready(function () {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Photographer.", "Artist.", "Custom furniture upholsterer."],
+		strings: ["Artist.", "Photographer.", "Backpack designer.", "Custom furniture upholsterer."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
 	});
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 5
+			}
+		}
+	})
 
 });
